33f9d6ae6abbbaa0e38c02cb0571f1cb575806b3
Marc-Andre <mandrelord@gmail.com>
2022-07-11 21:26:43 -0400

    Added exercises

--- rev_wstr.c
+++ rev_wstr.c
@@ -1,51 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_wstr.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malord <malord@student.42quebec.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 19:48:14 by malord            #+#    #+#             */
+/*   Updated: 2022/07/11 21:14:54 by malord           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int 	i = 0;
-	int		words = 1;
-	int		total_words = 0;
-	char	 array[4][5];
-
+	int len = 0;
+	int index = 0;
+	
 	if (argc == 2)
 	{
-		while (argv[1][i])
+		while (argv[1][len])
+			len++;
+		len--;
+		while (len >= 0)
 		{
-			if (argv[1][i] == ' ')
-				words++;
-			i++;
-		}
-		//array = malloc(sizeof(char *) * words + 1);
-		i = 0;
-		total_words = words;
-		words = 0;
-		while (words < total_words)
-		{
-			while (argv[1][i] != ' ')
+			while (argv[1][len] != ' ' && len >= 0)
+				len--;
+			index = len + 1;
+			while (argv[1][index] != ' ' && argv[1][index] != '\0')
 			{
-				array[words][i] = argv[1][i];
-				i++;
+				write (1, &argv[1][index], 1);
+				index++;
 			}
-			write(1, &array[words], strlen(array[words]));
-			write(1, " ", 1);
-			words++;
-			i++;
+			if (len > 0)
+				write (1, " ", 1);
+			len--;
 		}
-		/*words = 0;
-		i = 0;
-		while (array[words])
-		{
-			i = 0;
-			while (array[words][i])
-			{
-				write(1, &array[words][i], 1);
-				i++;
-			}
-			words++;
-		}*/
 	}
-	write (1, "\n", 1);
+	write(1, "\n", 1);
+	return (0);
 }
