9d48747e9d32b119d8f59cf43f2e69bfc6d1503d
thelord82 <mandrelord@gmail.com>
2022-07-12 14:03:38 -0400

    Added hidenp

--- ft_list_size.c
+++ ft_list_size.c
@@ -3,13 +3,14 @@
 #include <stdio.h>
 int	ft_list_size(t_list *begin_list)
 {
-	int i = 0;
-	while (begin_list)
+	int size = 0;
+	
+	while(begin_list)
 	{
-		i++;
 		begin_list = begin_list->next;
+		size++;
 	}
-	return (i);
+	return (size);
 }
 
 int main ()
@@ -18,14 +19,16 @@ int main ()
 	t_list *a = malloc(sizeof(t_list));
 	t_list *b = malloc(sizeof(t_list));
 	t_list *c = malloc(sizeof(t_list));
+	t_list *d = malloc(sizeof(t_list));
 
 	a->data = 3;
 	a->next = b;
 	b->data = 5;
 	b->next = c;
 	c->data = 7;
-	c->next = 0;
+	c->next = d;
+	d->data = 11;
+	d->next = 0;
 	head = a;
 	printf("Taille de liste : %d\n", ft_list_size(head));
 }
-
