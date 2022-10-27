1413dd922d8890491ad76e2032a3cadd472288be
thelord82 <mandrelord@gmail.com>
2022-10-27 14:44:15 -0400

    get_next_line 3 and 20 line versions

--- get_next_line.c
+++ get_next_line.c
@@ -5,32 +5,61 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: malord <malord@student.42quebec.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/24 08:16:31 by malord            #+#    #+#             */
-/*   Updated: 2022/10/25 15:26:21 by malord           ###   ########.fr       */
+/*   Created: 2022/10/27 14:21:55 by malord            #+#    #+#             */
+/*   Updated: 2022/10/27 14:41:59 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
 
 char *get_next_line(int fd)
 {
-    char *string = malloc(10000), *buffer = string;
-    while (read(fd, buffer, BUFFER_SIZE) > 0 && *buffer++ != '\n');
-    return (buffer > string) ? (*buffer = 0, string) : (free(string), NULL);
+	char *string = malloc(10000), *buffer = string;
+	while (read(fd, buffer, BUFFER_SIZE) > 0 && *buffer++ != '\n');
+	return (buffer > string) ? (*buffer = 0, string) : (free(string), NULL);
 }
+
+/*int ft_strlen(char *str)
+{
+	int i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char *get_next_line(int fd)
+{
+	char *string = NULL; 
+	char *buffer;
+
+	string = malloc(10000);
+	if (!string)
+		return (NULL);
+	buffer = string;
+	while (read(fd, buffer, BUFFER_SIZE) > 0 && *buffer++ != '\n');
+	if (buffer > string)
+	{
+		*buffer = 0;
+		return (string);
+	}
+	else
+	{
+		free (string);
+		return (NULL);	
+	}
+}*/
 
 int main(void)
 {
-    int fd = open("file", O_RDONLY);
-    char *line = get_next_line(fd);
+	int fd = open("file", O_RDONLY);
+	char *line = get_next_line(fd);
 
-    while (line != NULL)
-    {
-        printf("%s", line);
-        line = get_next_line(fd);
-    }
+	while (line != NULL)
+	{
+		printf("line = %s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	line = get_next_line(fd);
+	printf("line outside = %s", line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: malord <malord@student.42quebec.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 08:15:28 by malord            #+#    #+#             */
-/*   Updated: 2022/10/25 15:26:37 by malord           ###   ########.fr       */
+/*   Updated: 2022/10/27 14:42:44 by malord           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
 # define BUFFER_SIZE 1
 
 char	*get_next_line(int fd);
